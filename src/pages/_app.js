e044c609920af2f8bc740cc7c31ea663869233cf
Leonardo Belilo <leonardobelilo@outlook.com>
2024-07-11 09:50:40 -0300

    edit title and fivicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,8 +37,8 @@ const MyApp = ({ Component, ...rest }) => {
   return (
     <Fragment>
       <Head>
-        <title>Quarter - Real Estate NextJs Template</title>
-        <meta name="description" content="Quarter - Real Estate Nextjs Template" />
+        <title>Panum - Soluções imobiliarias</title>
+        <meta name="description" content="Panum - Soluções imobiliarias" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
